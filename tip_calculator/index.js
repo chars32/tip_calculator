4ae0326042aa7e1361394379cce8eaf9a4f29a25
Carlos Garcia Castellanos <chars24@gmail.com>
2021-09-17 11:26:06 -0500

    Logic working

--- tip_calculator/index.js
+++ tip_calculator/index.js
@@ -1,9 +1,12 @@
 let billInput = document.querySelector('#input__billBox')
 let peopleInput = document.querySelector('#input__peopleNumber')
 const percentageButton = document.querySelectorAll('.buttons')
+let bill = 0
+let people = 0
+let percentage = 0
 
 // Validate input value are numbers and not empty
-const validateNumber = (input) => {    
+const validateNumber = (input) => {
     if (input.value.length > 0) {
         if (isNaN(input.value) === true) {
             input.value = ''
@@ -17,17 +20,23 @@ const validateNumber = (input) => {
     } else {
         input.placeholder = 'Please enter a number'
         input.style.border = '1px solid red'
-        return false 
-    }  
+        return false
+    }
 }
+
+// Get tip total amount and by person
+const getTip = (bill, person, percentage) => {
+    let tip = (bill * percentage) / 100
+    let tipByPerson = (tip / person)
+    console.log(tip, tipByPerson)
+}
+
 
 percentageButton.forEach(button => {
     button.addEventListener('click', (e) => {
-        validateNumber(billInput) ?
-            console.log('billInput is valid') :
-            console.log('billInput is invalid')
-        validateNumber(peopleInput) ?
-            console.log('peopleInput is valid') :
-            console.log('peopleInput is invalid')
+        percentage = parseInt(e.target.value)
+        validateNumber(billInput) && (bill = parseInt(billInput.value))
+        validateNumber(peopleInput) && (people = parseInt(peopleInput.value))
+        bill && people && getTip(bill, people, percentage)
     })
 })
