d577be6c4d51f3312b0c98cf65e506fcfbc0f68f
Carlos Garcia Castellanos <chars24@gmail.com>
2021-09-16 23:03:28 -0500

    Validation input logic done

--- tip_calculator/index.js
+++ tip_calculator/index.js
@@ -2,28 +2,32 @@ let billInput = document.querySelector('#input__billBox')
 let peopleInput = document.querySelector('#input__peopleNumber')
 const percentageButton = document.querySelectorAll('.buttons')
 
-// Validate input value are numbers
-billInput.onkeyup = function (e) {
-    if (billInput.value.length > 0) {
-        isNaN(billInput.value) ?
-            console.log('no es numero') :
-            console.log("si es numero")
-    }
+// Validate input value are numbers and not empty
+const validateNumber = (input) => {    
+    if (input.value.length > 0) {
+        if (isNaN(input.value) === true) {
+            input.value = ''
+            input.placeholder = 'Please enter a number'
+            input.style.border = '1px solid red'
+            return false
+        } else {
+            input.style.border = 'none'
+            return true
+        }
+    } else {
+        input.placeholder = 'Please enter a number'
+        input.style.border = '1px solid red'
+        return false 
+    }  
 }
 
-// const validateNumber = (input) => {
-//     input.onkeyup = () => {
-//         isNaN(input.value) ? console.log('no es numero') : console.log("si es numero")
-//     }
-// }
-
-// percentageButton.forEach(button => {
-//     button.addEventListener('click', (e) => {
-//         billInput.value && peopleInput.value ? console.log('si') : console.log('no')
-        // const percentage = parseInt(e.target.innerText)
-        // const tip = billInput.value * percentage / 100
-        // const total = parseInt(billInput.value) + tip
-        // document.querySelector('#input__tipBox').value = tip.toFixed(2)
-        // document.querySelector('#input__totalBox').value = total.toFixed(2)
-//     })
-// })
+percentageButton.forEach(button => {
+    button.addEventListener('click', (e) => {
+        validateNumber(billInput) ?
+            console.log('billInput is valid') :
+            console.log('billInput is invalid')
+        validateNumber(peopleInput) ?
+            console.log('peopleInput is valid') :
+            console.log('peopleInput is invalid')
+    })
+})
